1cd732743d78fcbdcf8f84acd1490a1ea32c2915
Zhou <David.Zhou@ds.nrc.ca>
2024-05-10 10:17:46 -0400

    DEV: Multi-camera board builder and renderer working

--- src/gui/panels/__init__.py
+++ src/gui/panels/__init__.py
@@ -1,4 +1,5 @@
 from .base_panel import BasePanel
+from .board_builder_panel import BoardBuilderPanel
 from .calibrator_panel import CalibratorPanel
 from .connector_panel import ConnectorPanel
 from .detector_panel import DetectorPanel
