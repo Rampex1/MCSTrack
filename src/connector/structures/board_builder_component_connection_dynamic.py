fc69da8ad16910eafbe7226246c790641f249587
David Zhou <david.zhou.0110@gmail.com>
2024-06-12 14:06:07 -0400

    Deleted useless files

--- src/connector/structures/board_builder_component_connection_dynamic.py
+++
@@ -1,6 +0,0 @@
-from .base_component_connection_dynamic import BaseComponentConnectionDynamic
-
-
-class BoardBuilderComponentConnectionDynamic(BaseComponentConnectionDynamic):
-    def __init__(self):
-        super().__init__()
